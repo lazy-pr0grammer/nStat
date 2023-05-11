1790b939eed2c5cf79dc49831284f526a2228c2e
LazyProgrammer <lazyprogrammer21@gmail.com>
2023-05-12 00:44:21 +0600

    initial commit

--- app/src/main/java/com/aylax/nstat/ui/main/MainActivity.java
+++ app/src/main/java/com/aylax/nstat/ui/main/MainActivity.java
@@ -1,12 +1,14 @@
 package com.aylax.nstat.ui.main;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import com.aylax.nstat.R;
 import com.aylax.nstat.databinding.ActivityMainBinding;
 import com.aylax.nstat.util.Utils;
-import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
   private ActivityMainBinding binding;
@@ -23,9 +25,29 @@ public class MainActivity extends AppCompatActivity {
           .setStatusBarColor(ContextCompat.getColor(this, R.color.md_theme_light_background));
     else
       getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.md_theme_dark_background));
-    binding.viewPager.setAdapter(new ViewpagerAdapter(getSupportFragmentManager(), getLifecycle()));
-    new TabLayoutMediator(
-            binding.tabLayout, binding.viewPager, ((tab, position) -> tab.setText(names[position])))
-        .attach();
+    // binding.viewPager.setAdapter(new ViewpagerAdapter(getSupportFragmentManager(),
+    // getLifecycle()));
+    /*new TabLayoutMediator(
+           binding.tabLayout, binding.viewPager, ((tab, position) -> tab.setText(names[position])))
+       .attach();
+
+    */
+  }
+
+  private void init() {
+    binding.bottomNavigationView.setOnItemSelectedListener(
+        new NavigationBarView.OnItemSelectedListener() {
+          @Override
+          public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+              case R.id.home:
+
+                break;
+              case R.id.logs:
+                break;
+            }
+            return false;
+          }
+        });
   }
 }
