a95008e43b2952ed10420484fd48570c1c7a7078
LazyProgrammer <lazyprogrammer21@gmail.com>
2023-05-14 01:05:12 +0600

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/aylax/nstat/ui/usage/UsageViewModel.kt
+++ app/src/main/java/com/aylax/nstat/ui/usage/UsageViewModel.kt
@@ -1,16 +1,14 @@
 package com.aylax.nstat.ui.usage
 
-import android.app.Application
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 import com.aylax.library.model.AppUsage
-import com.aylax.nstat.data.model.Usage
 import com.aylax.nstat.data.repository.UsageRepository
 
-class UsageViewModel(application: Application) : AndroidViewModel(application) {
-    private val repository = UsageRepository(application)
+class UsageViewModel : ViewModel() {
+    private val repository = UsageRepository()
 
-     fun getAll(): LiveData<List<AppUsage>> {
+    fun getAll(): LiveData<List<AppUsage>> {
         return repository.getAll()
     }
 
